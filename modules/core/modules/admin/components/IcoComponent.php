560f894053726b96221b617b6e20a337bc5d9bd4
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2022-04-03 16:16:00 +0800

    Добавил компонент для вставки иконок

---
+++ modules/core/modules/admin/components/IcoComponent.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * IcoComponent
+ * Компонент для вставки иконок
+ * @copyright Copyright (c) 2022 Eugene Andreev
+ * @author Eugene Andreev <gjhonic@gmail.com>
+ *
+ */
+class IcoComponent
+{
+
+    const VIEW_ICO = 'fa-eye';
+
+    /**
+     * Возвращает иконку глазика
+     * @return string
+     */
+    public static function view(): string
+    {
+        return self::layout(self::VIEW_ICO);
+    }
+
+    /**
+     * Шаблон вывода иконки
+     * @param string $classIco
+     * @return string
+     */
+    private static function layout(string $classIco): string
+    {
+        return '<i class="fa '. $classIco . '" aria-hidden="true"></i>';
+    }
+}
