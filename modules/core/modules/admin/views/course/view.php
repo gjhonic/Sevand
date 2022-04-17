90ba3ef3d0f75cfd36d35cd7fae1cb44171fc27a
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2022-04-17 17:42:09 +0800

    Функционал для курсов

--- modules/core/modules/admin/views/course/view.php
+++ modules/core/modules/admin/views/course/view.php
@@ -8,6 +8,7 @@ use yii\widgets\DetailView;
 /* @var $model app\modules\core\models\base\Course */
 
 $this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => Module::t('app', 'Bases'), 'url' => ['/admin/bases']];
 $this->params['breadcrumbs'][] = ['label' => Module::t('app', 'Courses'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
@@ -16,28 +17,5 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a(Module::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            [
-                'attribute' => 'created_at',
-                'value' => function ($model) {
-                    return date('j F, Y H:i:s', $model->created_at);
-                }
-            ],
-            [
-                'attribute' => 'updated_at',
-                'value' => function ($model) {
-                    return date('j F, Y H:i:s', $model->updated_at);
-                }
-            ]
-        ],
-    ]) ?>
-
+    <h2><?=Module::t('app', 'Groups')?>:</h2>
 </div>
