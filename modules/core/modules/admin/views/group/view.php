9a046bb02eb1d2e0dfeb1c3173475fdca4ed10a9
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2022-04-19 11:01:48 +0800

    комит

--- modules/core/modules/admin/views/group/view.php
+++ modules/core/modules/admin/views/group/view.php
@@ -12,6 +12,7 @@ use yii\widgets\DetailView;
 /* @var $model app\modules\core\models\base\Group */
 
 $this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => Module::t('app', 'Bases'), 'url' => ['/admin/bases']];
 $this->params['breadcrumbs'][] = ['label' => Module::t('app', 'Groups'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
@@ -21,101 +22,143 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?php if (Yii::$app->user->identity->role !== User::ROLE_MODERATOR) { ?>
-            <?= Html::a(IcoComponent::edit() . ' ' . Module::t('app', 'Edit'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-            <?= Html::a(IcoComponent::delete() . ' ' . Module::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => Module::t('note', 'Are you sure you want to delete this item?'),
-                    'method' => 'post',
-                ],
-            ]) ?>
-        <?php } ?>
+        <?php
+        if (Yii::$app->user->identity->role !== User::ROLE_MODERATOR) { ?>
+            <?= Html::a(
+                IcoComponent::edit() . ' ' . Module::t('app', 'Edit'),
+                ['update', 'id' => $model->id],
+                ['class' => 'btn btn-primary']
+            ) ?>
+            <?= Html::a(
+                IcoComponent::delete() . ' ' . Module::t('app', 'Delete'),
+                ['delete', 'id' => $model->id],
+                [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Module::t('note', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
+                ]
+            ) ?>
+        <?php
+        } ?>
         <?php
         if ($model->activity_id === Group::ACTIVITY_ENABLE_ID) {
-            echo Html::a(IcoComponent::disable() . ' ' . Module::t('app', 'To archive'), ['disable', 'id' => $model->id], [
-                'class' => 'btn btn-warning',
-                'data' => [
-                    'confirm' => Module::t('note', 'Are you sure you want to archive the discipline?'),
-                ],
-            ]);
+            echo Html::a(
+                IcoComponent::disable() . ' ' . Module::t('app', 'To archive'),
+                ['disable', 'id' => $model->id],
+                [
+                    'class' => 'btn btn-warning',
+                    'data' => [
+                        'confirm' => Module::t('note', 'Are you sure you want to archive the discipline?'),
+                    ],
+                ]
+            );
         } elseif ($model->activity_id === Group::ACTIVITY_DISABLE_ID) {
-            echo Html::a(IcoComponent::enable() . ' ' .Module::t('app', 'Activate'), ['enable', 'id' => $model->id], [
-                'class' => 'btn btn-warning',
-                'data' => [
-                    'confirm' => Module::t('note', 'Are you sure you want to activate the discipline?'),
-                ],
-            ]);
+            echo Html::a(
+                IcoComponent::enable() . ' ' . Module::t('app', 'Activate'),
+                ['enable', 'id' => $model->id],
+                [
+                    'class' => 'btn btn-warning',
+                    'data' => [
+                        'confirm' => Module::t('note', 'Are you sure you want to activate the discipline?'),
+                    ],
+                ]
+            );
         }
         ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            [
-                'attribute' => 'course_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    $course = $model->course;
-                    if($course){
-                        return Html::a($course->title,
-                                       Url::to(['admin/course/view', 'id' => $model->course_id]),
-                                       ['class' => 'btn btn-outline-secondary']);
+    <?= DetailView::widget(
+        [
+            'model' => $model,
+            'attributes' => [
+                'id',
+                'title',
+                [
+                    'attribute' => 'course_id',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        $course = $model->course;
+                        if ($course) {
+                            return Html::a(
+                                $course->title,
+                                Url::to(['course/view', 'id' => $model->course_id]),
+                                ['class' => 'btn btn-outline-secondary']
+                            );
+                        }
                     }
-                }
+                ],
+                [
+                    'attribute' => 'direction_id',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        $direction = $model->direction;
+                        if($direction){
+                            return Html::a(
+                                $direction->short_title,
+                                Url::to(['direction/view', 'id' => $model->direction_id]),
+                                ['class' => 'btn btn-outline-secondary']
+                            );
+                        }
+                    }
+                ],
+                [
+                    'attribute' => 'curator_id',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        $curator = $model->curator;
+                        if ($curator) {
+                            return Html::a(
+                                $curator->username,
+                                Url::to(['user/view', 'id' => $model->curator_id]),
+                                ['class' => 'btn btn-outline-secondary']
+                            );
+                        }
+                    }
+                ],
+                [
+                    'attribute' => 'headman_id',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        $headman = $model->headman;
+                        if ($headman) {
+                            return Html::a(
+                                $headman->username,
+                                Url::to(['user/view', 'id' => $model->headman_id]),
+                                ['class' => 'btn btn-outline-secondary']
+                            );
+                        }
+                    }
+                ],
+                [
+                    'attribute' => 'department_id',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        $department = $model->department;
+                        if ($department) {
+                            return Html::a(
+                                $department->short_title,
+                                Url::to(['department/view', 'id' => $model->department_id]),
+                                ['class' => 'btn btn-outline-secondary']
+                            );
+                        }
+                    }
+                ],
+                [
+                    'attribute' => 'created_at',
+                    'value' => function ($model) {
+                        return Yii::$app->formatter->asDatetime($model->created_at, "php:d.m.Y H:i:s");
+                    }
+                ],
+                [
+                    'attribute' => 'updated_at',
+                    'value' => function ($model) {
+                        return Yii::$app->formatter->asDatetime($model->updated_at, "php:d.m.Y H:i:s");
+                    }
+                ]
             ],
-            [
-                'attribute' => 'direction_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return Html::a($model->direction->short_title,
-                        Url::to(['admin/direction/view', 'id' => $model->direction_id]),
-                        ['class' => 'btn btn-outline-secondary']);
-                }
-            ],
-            [
-                'attribute' => 'curator_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return Html::a($model->curator->username,
-                        Url::to(['admin/user/view', 'id' => $model->curator_id]),
-                        ['class' => 'btn btn-outline-secondary']);
-                }
-            ],
-            [
-                'attribute' => 'headman_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return Html::a($model->headman->username,
-                        Url::to(['admin/user/view', 'id' => $model->headman_id]),
-                        ['class' => 'btn btn-outline-secondary']);
-                }
-            ],
-            [
-                'attribute' => 'department_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return Html::a($model->department->short_title,
-                        Url::to(['admin/department/view', 'id' => $model->department_id]),
-                        ['class' => 'btn btn-outline-secondary']);
-                }
-            ],
-            [
-                'attribute' => 'created_at',
-                'value' => function ($model) {
-                    return Yii::$app->formatter->asDatetime($model->created_at, "php:d.m.Y H:i:s");
-                }
-            ],
-            [
-                'attribute' => 'updated_at',
-                'value' => function ($model) {
-                    return Yii::$app->formatter->asDatetime($model->updated_at, "php:d.m.Y H:i:s");
-                }
-            ]
-        ],
-    ]) ?>
+        ]
+    ) ?>
 
 </div>
